ba32efd133aa8709376ff2e925f6df245f65f2b3
Lartsomihaili <156167165+Lartsomihaili@users.noreply.github.com>
2024-11-03 19:24:45 +0200

    Create IMessageRepository.cs

---
+++ IMessageRepository.cs
@@ -0,0 +1,19 @@
+using sovelluksenbackend.Models;
+
+namespace Backend2024.Repositories
+{
+    public interface IMessageRepository
+    {
+
+        Task<IEnumerable<Message>> GetMessagesAsync();
+        Task<IEnumerable<Message>> GetMySentMessagesAsync(User user);
+        Task<IEnumerable<Message>> GeMyReceivedMessagesAsync(User user);
+        Task<IEnumerable<Message>> GetMessagesAsync(long id);
+        Task<Message?> GetMessageAsync(long id);
+        Task<Message?> NewMessageAsync(Message message);
+        Task<bool> UpdateMessageAsync(Message message);
+        Task<bool> DeleteMessageAsync(Message message);
+
+        
+    }
+}
