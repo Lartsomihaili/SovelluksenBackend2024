147c4a3e361dbfe916663c0e05e57bb5ea1616d0
Lartsomihaili <156167165+Lartsomihaili@users.noreply.github.com>
2024-11-03 19:27:53 +0200

    Create Message.cs

---
+++ Message.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace sovelluksenbackend.Models
+{
+    public class Message
+    {
+        public long Id { get; set; }
+        [MaxLength(100)]
+
+        public string Title { get; set; }
+        [MaxLength(1000)]
+
+        public string? Body { get; set; }
+
+        public User Sender { get; set; }
+
+        public User? Recipient { get; set; }
+
+        public Message? PrevMessage {  get; set; }
+        
+
+    }
+
+    public class MessageDTO
+    {
+        public long Id { get; set; }
+        [MaxLength(100)]
+
+        public string Title { get; set; }
+        [MaxLength(1000)]
+
+        public string? Body { get; set; }
+
+        public string Sender { get; set; }
+
+        public string? Recipient { get; set; }
+
+        public long PrevMessage { get; set; }
+    }
+
+}
