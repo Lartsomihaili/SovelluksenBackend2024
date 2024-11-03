799b997c02d49df8802247c6d4019a6a879eff3c
Lartsomihaili <156167165+Lartsomihaili@users.noreply.github.com>
2024-11-03 19:20:15 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,67 @@
+using Backend2024.Repositories;
+using Microsoft.EntityFrameworkCore;
+using sovelluksenbackend.Models;
+using Backend2024.Services;
+using Microsoft.AspNetCore.Authentication;
+using sovelluksenbackend.Middleware;
+using Microsoft.OpenApi.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<MessageServiceContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MessageDB")));
+builder.Services.AddAuthentication("BasicAuthentication").AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Message API",
+        Description = "An ASP.NET Core Web API for sending and receiving messages",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
+builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IMessageService, MessageService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserAuthenticationService, UserAuthenticationService>();
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    MessageServiceContext dbcontext = scope.ServiceProvider.GetRequiredService<MessageServiceContext>();
+    dbcontext.Database.EnsureCreated();
+
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseMiddleware<ApiKeyMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
